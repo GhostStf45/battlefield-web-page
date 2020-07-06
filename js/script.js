b943145b497d23f269eda264b448f7bb1721e9e7
Stefano Gomez <topguncounter@hotmail.com>
2020-07-05 20:09:35 -0500

    Demo finished 5-07-2020

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
+ 'use strict'
+ window.sr = ScrollReveal();
+ sr.reveal('.navbar-brand',{
+    duration: 2000,
+    origin: 'left',
+    distance: '300px'
+ })
+ /* Alert info animation */
+ sr.reveal('.alert-game',{
+    duration: 2000,
+    origin: 'right',
+    distance: '300px'
+ });
+ /* company logos */
+ sr.reveal('.secondary-title',{
+   duration: 2000,
+   origin:'left',
+   distance: '300px'
+ });
+ sr.reveal('.logo-normal-size',{
+    duration: 2000,
+    origin:'bottom',
+    distance: '300px'
+ });
  // Add slideDown animation to Bootstrap dropdown when expanding.
  $('.dropdown').on('show.bs.dropdown', function() {
     $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
